bc81dfe8a0850e0d74414afe899c941ec7fea833
pallomabritoGN <palloma.brito@gerencianet.com.br>
2022-08-11 10:03:11 -0300

    Inclusão de novos exemplos de chamadas

--- Gerencianet.NETCore.SDK.Examples/Program.cs
+++ Gerencianet.NETCore.SDK.Examples/Program.cs
@@ -1,89 +1,122 @@
-﻿namespace Gerencianet.NETCore.SDK.Examples {
-    internal class Program {
-        private static void Main (string[] args) {
+﻿using System;
 
-            /* PIX - WEBHOOK*/
-            //PixConfigWebhook.Execute();
-            //PixDeleteWebhook.Execute();
-            //PixDetailWebhook.Execute();
-            //PixListWebhook.Execute();
+namespace Gerencianet.NETCore.SDK.Examples
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /* PIX - WEBHOOK */
+            // PixConfigWebhook.Execute();
+            // PixDeleteWebhook.Execute();
+            // PixDetailWebhook.Execute();
+            // PixListWebhook.Execute();
             
-            /* PIX - CHARGE*/
-            //PixCreateCharge.Execute();
-            //PixCreateImmediateCharge.Execute();
-            //PixDetailCharge.Execute();
-            //PixListCharges.Execute();
-            //PixUpdateCharge.Execute();
-            
-            /* PIX - PIX*/
-            //PixDetail.Execute();
-            //PixDevolution.Execute();
-            //PixDetailDevolution.Execute();
-            //PixListReceived.Execute();
-            //PixSend.Execute();
-            
-            /* PIX - LOCATION*/
-            //PixCreateLocation.Execute();
-            //PixDetailLocation.Execute();
-            //PixListLocation.Execute();
-            //PixUnsetTxid.Execute();
-            //PixGenerateQRCode.Execute();
+            /* PIX - CHARGE */
+            // PixCreateCharge.Execute();
+            // PixCreateImmediateCharge.Execute();
+            // PixDetailCharge.Execute();
+            // PixListCharges.Execute();
+            // PixUpdateCharge.Execute();
 
-            /* GN - KEY*/
-            //GnListEvp.Execute();
-            //GnCreateEvp.Execute();
-            //GnDeleteEvp.Execute();
+            /* PIX - COBV */
+            // PixCreateDueCharge.Execute();
+            // PixUpdateDueCharge.Execute();
+            // PixDetailDueCharge.Execute();
+            // PixListDueCharges.Execute();
+            
+            /* PIX - PIX */
+            // PixDetail.Execute();
+            // PixDevolution.Execute();
+            // PixDetailDevolution.Execute();
+            // PixListReceived.Execute();
+            // PixSend.Execute();
+            // PixSendPut.Execute();
+            
+            /* PIX - LOCATION */
+            // PixCreateLocation.Execute();
+            // PixDetailLocation.Execute();
+            // PixListLocation.Execute();
+            // PixUnsetTxid.Execute();
+            // PixGenerateQRCode.Execute();
 
-            /* GN - ACCOUNT*/
-            //GnDetailBalance.Execute();
-            //GnDetailSettings.Execute();
-            //GnUpdateSettings.Execute();
+            /* GN - KEY */
+            // GnListEvp.Execute();
+            // GnCreateEvp.Execute();
+            // GnDeleteEvp.Execute();
+
+            /* GN - ACCOUNT */
+            // GnDetailBalance.Execute();
+            // GnDetailSettings.Execute();
+            // GnUpdateSettings.Execute();
+
+            /* GN - REPORT */
+            // CreateReport.Execute();
+            // DetailReport.Execute();
 
             /* CHARGES */
-            //OneStepBankingBillet.Execute ();
-            //CreateCharge.Execute();
-            //CreateChargeHistory.Execute();
-            //DetailCharge.Execute();
-            //ResendBillet.Execute();
-            //UpdateBillet.Execute();
-            //UpdateChargeMetadata.Execute();
-            //CancelCharge.Execute();
-            //SettleCharge.Execute();
-            //CreateBilletPayment.Execute();
-            //CreateCardPayment.Execute();
-            //ChargeLink.Execute();
+            // OneStepBankingBillet.Execute ();
+            // CreateCharge.Execute();
+            // CreateChargeHistory.Execute();
+            // DetailCharge.Execute();
+            // ResendBillet.Execute();
+            // UpdateBillet.Execute();
+            // UpdateChargeMetadata.Execute();
+            // CancelCharge.Execute();
+            // SettleCharge.Execute();
+            // CreateBilletPayment.Execute();
+            // CreateCardPayment.Execute();
+            // ChargeLink.Execute();
+            // OneStepLink.Execute();
+            // ResendChargeLink.Execute();
 
             /* SUBSCRIPTIONS */
-            //CancelSubscription.Execute();
-            //CreatePlan.Execute();
-            //GetPlans.Execute();
-            //DeletePlan.Execute();
-            //CreateSubscription.Execute();
-            //CreateSubscriptionHistory.Execute();
-            //CreateSubscriptionPayment.Execute();
-            //DetailSubscription.Execute();
-            //UpdateSubscriptionMetadata.Execute();
-            //UpdatePlan.Execute();
+            // CancelSubscription.Execute();
+            // CreatePlan.Execute();
+            // GetPlans.Execute();
+            // DeletePlan.Execute();
+            // CreateSubscription.Execute();
+            // CreateSubscriptionHistory.Execute();
+            // CreateSubscriptionPayment.Execute();
+            // DetailSubscription.Execute();
+            // UpdateSubscriptionMetadata.Execute();
+            // UpdatePlan.Execute();
+            // OneStepSubscriptionLink.Execute();
+            // OneStepSubscription.Execute();
+            // ResendSubscriptionCharge.Execute();
 
             /* CARNETS */
-            //CancelCarnet.Execute();
-            //CancelParcel.Execute();
-            //CreateCarnet.Execute();
-            //CreateCarnetHistory.Execute();
-            //DetailCarnet.Execute();
-            //ResendCarnet.Execute();
-            //ResendParcel.Execute();
-            //SettleCarnetParcel.Execute();
-            //UpdateCarnetMetadata.Execute();
-            //UpdateParcel.Execute();
+            // CancelCarnet.Execute();
+            // CancelParcel.Execute();
+            // CreateCarnet.Execute();
+            // CreateCarnetHistory.Execute();
+            // DetailCarnet.Execute();
+            // ResendCarnet.Execute();
+            // ResendParcel.Execute();
+            // SettleCarnet.Execute();
+            // SettleCarnetParcel.Execute();
+            // UpdateCarnetMetadata.Execute();
+            // UpdateParcel.Execute();
 
             /* NOTIFICATIONS */
             //GetNotification.Execute();
 
             /* OTHER */
-            //GetInstallments.Execute();
+            // GetInstallments.Execute();
 
-            //AllInOne.Execute();
+            /* OPEN FINANCE */
+            // OfUpdateSettings.Execute();
+            // OfDetailSettings.Execute();
+            // OfListParticipants.Execute();
+            // OfStartPixPayment.Execute();
+            // OfListPixPayment.Execute();
+
+            /* PAYMENTS */
+            // PayDetailBarCode.Execute();
+            // PayRequestBarCode.Execute();
+            // PayDetailPayment.Execute();
+            // PayListPayments.Execute();
+
         }
     }
 }
